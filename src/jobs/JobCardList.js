312e529bb0195522017e880261fc645d1c42d8f6
eduardopedroza <ea.pedroza23@gmail.com>
2024-03-06 20:39:21 -0600

    created jobcardlist

---
+++ src/jobs/JobCardList.js
@@ -0,0 +1,14 @@
+import React from "react";
+import JobCard from "./JobCard";
+
+function JobCardList({ jobs }) {
+  return (
+    <div className="JobCardList">
+      {jobs.map((job) => (
+        <JobCard job={job} />
+      ))}
+    </div>
+  );
+}
+
+export default JobCardList;
