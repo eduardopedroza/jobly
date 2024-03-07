efc87b0687400de6b612407847c161f0499fdb70
eduardopedroza <ea.pedroza23@gmail.com>
2024-03-06 20:37:39 -0600

    created navbar and routes

--- src/routes-nav/Navigation.js
+++ src/routes-nav/Navigation.js
@@ -7,48 +7,54 @@ function Navigation({ logout }) {
   const { currentUser } = useContext(UserContext);
 
   function loggedInNav() {
-    <>
-      <Nav>
-        <NavItem>
-          <NavLink to="/companies">Companies</NavLink>
-        </NavItem>
-      </Nav>
-      <Nav>
-        <NavItem>
-          <NavLink to="/jobs">Jobs</NavLink>
-        </NavItem>
-      </Nav>
-      <Nav>
-        <NavItem>
-          <NavLink to="/profile">Profile</NavLink>
-        </NavItem>
-      </Nav>
-      <Nav>
-        <NavItem>
-          <NavLink to="/">Logout</NavLink>
-        </NavItem>
-      </Nav>
-      <Nav>
-        <NavItem>
-          <span>Welcome, {currentUser.username}</span>
-        </NavItem>
-      </Nav>
-    </>;
+    return (
+      <>
+        <Nav>
+          <NavItem>
+            <NavLink to="/companies">Companies</NavLink>
+          </NavItem>
+        </Nav>
+        <Nav>
+          <NavItem>
+            <NavLink to="/jobs">Jobs</NavLink>
+          </NavItem>
+        </Nav>
+        <Nav>
+          <NavItem>
+            <NavLink to="/profile">Profile</NavLink>
+          </NavItem>
+        </Nav>
+        <Nav>
+          <NavItem>
+            <NavLink to="/" onClick={logout}>
+              Logout
+            </NavLink>
+          </NavItem>
+        </Nav>
+        <Nav>
+          <NavItem>
+            <span>Welcome, {currentUser.username}</span>
+          </NavItem>
+        </Nav>
+      </>
+    );
   }
 
   function loggedOutNav() {
-    <>
-      <Nav>
-        <NavItem>
-          <NavLink to="/login">Login</NavLink>
-        </NavItem>
-      </Nav>
-      <Nav>
-        <NavItem>
-          <NavLink to="/signup">Sign Up</NavLink>
-        </NavItem>
-      </Nav>
-    </>;
+    return (
+      <>
+        <Nav>
+          <NavItem>
+            <NavLink to="/login">Login</NavLink>
+          </NavItem>
+        </Nav>
+        <Nav>
+          <NavItem>
+            <NavLink to="/signup">Sign Up</NavLink>
+          </NavItem>
+        </Nav>
+      </>
+    );
   }
 
   return (
--- src/routes-nav/Routes.js
+++ src/routes-nav/Routes.js
@@ -1,20 +1,26 @@
 import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import PrivateRoute from "./PrivateRoute";
+import Homepage from "../homepage/Homepage";
+import CompanyList from "../companies/CompanyList";
+import CompanyDetail from "../companies/CompanyDetail";
+import JobList from "../jobs/JobList";
+import ProfileForm from "../profiles/ProfileForm";
+import LoginForm from "../auth/LoginForm";
+import SignupForm from "../auth/SignupForm";
 
-function Routes({ login, signup }) {
+function MyRoutes({ login, signup }) {
   return (
     <Routes>
       <Route path="/" element={<Homepage />} />
-      <PrivateRoute path="/companies" element={<CompanyList />} />
-      <PrivateRoute path="/companies/:handle" element={<CompanyDetail />} />
-      <PrivateRoute path="/jobs" element={<JobList />} />
-      <PrivateRoute path="/profile" element={<ProfileForm />} />
+      <Route path="/companies" element={<CompanyList />} />
+      <Route path="/companies/:handle" element={<CompanyDetail />} />
+      <Route path="/jobs" element={<JobList />} />
+      <Route path="/profile" element={<ProfileForm />} />
       <Route path="/login" element={<LoginForm login={login} />} />
       <Route path="/signup" element={<SignupForm signup={signup} />} />
-      <Navigate to="/" />
     </Routes>
   );
 }
 
-export default Routes;
+export default MyRoutes;
